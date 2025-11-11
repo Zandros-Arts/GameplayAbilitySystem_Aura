cc0d51ab839d916d787f7fbb914c16bd8be897e7
Zandros-Arts <137920663+Zandros-Arts@users.noreply.github.com>
2025-11-11 17:28:31 -0500

    Added `AuraAbilitySystemComponent`, `AuraAttributeSet`, and `AuraPlayerState` with integrated ability system functionality

---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright BigNasty
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,22 @@
+// Copyright BigNasty
+
+
+#include "Player/AuraPlayerState.h"
+
+#include "AttributeSet.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAttributeSet>("AttributeSet");
+	
+	NetUpdateFrequency = 100.f;
+}
+
+UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
