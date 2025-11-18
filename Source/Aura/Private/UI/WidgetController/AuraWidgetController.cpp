f8e3df1d71b6afe98cc61356534d94ca15a10312
Zandros-Arts <137920663+Zandros-Arts@users.noreply.github.com>
2025-11-18 17:20:16 -0500

    Added `BroadcastInitialValues` functionality: implemented in `OverlayWidgetController` to broadcast health and max health attributes, updated `AuraHUD` to call this method during overlay initialization, and extended `AuraWidgetController` for override support. Updated related widget and blueprint assets.

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -3,11 +3,18 @@
 
 #include "UI/WidgetController/AuraWidgetController.h"
 
+#include "UI/Widget/AuraUserWidget.h"
+
 void UAuraWidgetController::SetWidgetControllerParams(const FWidgetControllerParams WCParams)
 {
 	PlayerController = WCParams.PlayerController;
 	PlayerState = WCParams.PlayerState;
 	AbilitySystemComponent = WCParams.AbilitySystemComponent;
 	AttributeSet = WCParams.AttributeSet;
+	
+}
+
+void UAuraWidgetController::BroadcastInitialValues()
+{
 	
 }
